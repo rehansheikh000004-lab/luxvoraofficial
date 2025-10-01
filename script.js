076892129e82c8507c9293d10fa2f62edcf4e61d
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2025-10-01 12:26:06 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function showTab(tabId) {
+  document.querySelectorAll('.tab').forEach(tab => {
+    tab.classList.remove('active');
+  });
+  document.getElementById(tabId).classList.add('active');
+}
+
+function loadProject(project) {
+  const frame = document.getElementById("projectFrame");
+  frame.src = project + ".html";
+  showTab("projects");
+}
